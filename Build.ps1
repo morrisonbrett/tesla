56a9be4e6634ca1069e5373a409d925869db3f14
Brett Morrison <b@brettmorrison.com>
2016-07-02 10:48:00 -0700

    Updated Build script to force Visual Studio 2015 version of MSBuild Thanks for the tip, Andrew (https://andrewlock.net/publishing-your-first-nuget-package-with-appveyor-and-myget/#comment-2762177520)

--- Build.ps1
+++ Build.ps1
@@ -54,7 +54,8 @@ EnsurePsbuildInstalled
 
 exec { & dotnet restore }
 
-# Invoke-MSBuild
+Set-MsBuild "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
+Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
